448ca1a42f7d5d220fdecc86af752a786be7ad70
Bruno Casali <brunoocasali@gmail.com>
2016-05-03 02:13:42 -0300

    update nav bar and redirect from devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
-  resources :leads
   devise_for :users
+
+  authenticated :user do
+    root 'leads#index', as: :authenticated_root
+  end
+
+  resources :leads
+
+  root to: redirect('/users/sign_in')
 end
