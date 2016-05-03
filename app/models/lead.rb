7eb2f05b7b45a48d0ea8e516af49ec401872efc9
Bruno Casali <brunoocasali@gmail.com>
2016-05-03 02:00:42 -0300

    created a good default crud templates

--- app/models/lead.rb
+++ app/models/lead.rb
@@ -1,2 +1,7 @@
 class Lead < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+
+  has_enumeration_for :status, with: CommonStatus, required: true
 end
