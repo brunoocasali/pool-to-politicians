f1b5ff41df7a5c665f200da7a414bde7381a9b26
Bruno Casali <brunoocasali@gmail.com>
2016-05-30 22:00:55 -0300

    added delivery form

--- app/forms/delivery_form.rb
+++ app/forms/delivery_form.rb
@@ -1,5 +1,9 @@
 class DeliveryForm
   include ActiveModel::Model
 
-  attr_accessor :delivery_content, :leads
+  attr_accessor :state, :city, :delivery_content, :leads
+
+  def self.model_name
+    ActiveModel::Name.new self, nil, 'Delivery'
+  end
 end
